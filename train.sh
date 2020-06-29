6ec3d0052a7693f1ad0a705bdb546464803c5b8a
Xintao <31618212+Hsintao@users.noreply.github.com>
2020-06-29 10:20:51 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -4,5 +4,5 @@ experiments/seg_detector/totaltext_resnet50_deform_thre.yaml --num_gpus 2 --resu
 #CUDA_VISIBLE_DEVICES=4 python  train.py experiments/seg_detector/totaltext_resnet50_deform_thre.yaml --num_gpus 1
 
 CUDA_VISIBLE_DEVICES=4 python  train.py experiments/seg_detector/1.yaml --num_gpus 1 --resume pretrained/totaltext_resnet50
-CUDA_VISIBLE_DEVICES=4 python eval.py experiments/seg_detector/totaltext_resnet50_deform_thre.yaml --polygon --box_thresh 0.6 \
+CUDA_VISIBLE_DEVICES=5 python eval.py experiments/seg_detector/totaltext_resnet50_deform_thre.yaml --polygon --box_thresh 0.6 \
 --resume  workspace/SegDetectorModel-seg_detector/deformable_resnet50/L1BalanceCELoss/model_lr1e-3_epoch200/model_epoch_
